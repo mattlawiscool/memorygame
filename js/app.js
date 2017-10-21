449fccc2c3195f32f2e95652fabda56777b38c47
mattlawiscool <mattlawiscool@gmail.com>
2017-10-20 19:01:25 -0700

    This is the beginnings to my memory game
    Shuffle button shuffles cards. Working. Need to add if statements to
    make the game work. :)

---
+++ js/app.js
@@ -0,0 +1,90 @@
+/* 
+10.19
+Added code to write into the class of the cards. Need to find a way to loop through each possibly "this"
+
+
+*/
+
+
+
+/*
+ * Create a list that holds all of your cards
+ */
+let deck = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bomb', 'fa fa-bicycle', 'fa fa-bicycle']
+
+let cards = $(".card").children()
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+// Listener Function
+
+
+
+function cardClick() {
+    card = $('.card')
+    $(card).click(function() {
+        $(this).attr('class', 'card open show remove');
+    });
+}
+
+cardClick()
+
+function newCards() {
+    let decks = "";
+    let newCard = "";
+    $(".remove").remove();
+    for (var i = 0; i < deck.length; i++) {
+        newCard += `<li class="card remove"><i class="${deck[i]} remove"></i></li>`;
+
+
+        /*decks = deck[i]
+        cards.attr('class', decks);*/
+
+    }
+
+    $(".deck").append(newCard)
+}
+
+function shuffleDeck() {
+    reset = $('.restart');
+    $(reset).click(function() {
+        alert(`You shuffled the deck!`)
+        shuffle(deck);
+        newCards();
+        cardClick();
+    });
+}
+shuffleDeck();
